db15331d860881778e73a3333eb06ba173c0837a
David Berger <david.berger@dotag.ch>
2026-06-14 09:31:21 +0200

    Initial Commit

---
+++ schreibwerkstatt-focuseditor/schreibwerkstatt_focuseditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  schreibwerkstatt_focuseditorApp.swift
+//  schreibwerkstatt-focuseditor
+//
+//  Created by David Berger on 14.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct schreibwerkstatt_focuseditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
